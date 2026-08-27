d1e44eccfe09adb53287a54d0a37dea8953fc463
Abdelhak-Djillali <abdelhak.djillali99@gmail.com>
2026-08-27 16:46:11 +0100

    Solve:Check if a Number is Prime (easy/basic-syntax)

---
+++ easy/check-if-a-number-is-prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+def is_prime(n):
+    if n < 2:
+        return False
+    for i in range(2, int(n ** 0.5) + 1):
+        if n % i == 0:
+            return False
+    return True
+
+print(is_prime(17))  # True
+print(is_prime(18))  # False
+print(is_prime(1))   # False
